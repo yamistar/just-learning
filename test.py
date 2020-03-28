51cee99b51dca2e35f9fb788508fab231bc647fe
yumit <yamistart@gmail.com>
2020-03-28 23:20:32 +0900

    커밋 메시지를 뭘로 할까나 요요요 예예예

--- test.py
+++ test.py
@@ -1,1 +1,30 @@
 print("hello world")
+
+
+family = ['mother', 'father', 'gentleman', 'sexy lady']
+
+family.remove("gentleman")
+print(family)
+
+
+
+num = 1
+while num <= 100:
+    print(num)
+    num = num + 1
+
+
+a= 1234
+b= 123
+
+if a > b:
+    print("a")
+else:
+    print("b")
+
+
+#### for sentence
+
+
+for x in family:
+    print( x)
