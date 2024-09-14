f18c652b2bbbdcf95745e6552d3a81eaf1a0f17f
yumix <58537229+yamistar@users.noreply.github.com>
2024-09-14 23:57:10 +0900

    Update test.py
    첫번째 줄 추가했어 - 기존 main 브랜치에 바로 반영할것이다

--- getting_started/test.py
+++ getting_started/test.py
@@ -1,18 +1,16 @@
-print("hello world")
+# Hello python
 
+print("hello world")
 
 family = ['mother', 'father', 'gentleman', 'sexy lady']
 
 family.remove("gentleman")
 print(family)
 
-
-
 num = 1
 while num <= 100:
     print(num)
     num = num + 1
-
 
 a= 1234
 b= 123
@@ -22,9 +20,7 @@ if a > b:
 else:
     print("b")
 
-
 #### for sentence
-
 
 for x in family:
     print( x)
